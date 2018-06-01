9eae1857918cac0449784167f6291b4f68b3ddf6
Adam Grandquist <grandquista@gmail.com>
2018-05-31 19:25:52 -0700

    WIP

--- test_kata.py
+++ test_kata.py
@@ -1,14 +1,17 @@
-from kata import kata
 from re import search
+
+from kata import kata
 
 
 def test_kata():
-    i = 1
-    for j in range(200):
+    """
+    Test kata.
+    """
+    for j in range(2000):
         sample = bin(j)[2:]
         for i in range(1, 19):
             match = search(kata(i), sample)
             if j % i == 0:
-                assert match, (i, j)
+                assert match, (i, j, bin(j))
             else:
                 assert not match, (i, j)
